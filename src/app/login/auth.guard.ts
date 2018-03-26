117b4ce66157df41e098b2cfc269ebb9acb4f276
CarlosMarcel <carlos.strike.quesada@gmail.com>
2018-03-26 12:24:30 -0600

    Commit #4
    Cambio de posible fallo de recarga y no verificacion si esta logeado

--- src/app/login/auth.guard.ts
+++ src/app/login/auth.guard.ts
@@ -12,9 +12,9 @@ export class AuthGuard implements CanActivate{
     canActivate(){
         if(this.auth.isLoggedIn()){
             return true;
-        }
-
-        this.router.navigate(['/login']);
-        return false;
+        }else{
+            this.router.navigate(['/login']);
+            return false;
+        }        
     }
 }
